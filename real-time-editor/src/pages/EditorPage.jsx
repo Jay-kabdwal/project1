1974886d2543ad0e5761892d31e883d8c5f43b63
jay-kabdwal <jaykabdwal@gmail.com>
2024-12-26 22:41:00 +0530

    backend

--- real-time-editor/src/pages/EditorPage.jsx
+++ real-time-editor/src/pages/EditorPage.jsx
@@ -1,8 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Client from "../component/Client";
 import Editor from "../component/Editor";
+import { initsocket } from "../../../socket";
+import { useLocation } from 'react-router-dom';
 
 const EditorPage = () => {
+
+    const socketRef = useRef(null);
+    const location = useLocation();
+    useEffect(() => {
+        const init = async () => {
+            socketRef.current = await initsocket();
+            socketRef.current.emit(ACTIONS.JOIN,{
+                roomId,
+                username: location.state?.username,
+            });
+        };
+        init();
+    },[]);
 
     const [clients, setclients] = useState([
         { socketId: 1, username: 'rakesh k' },
